319fb70ac74c8d2d108f1a3d6b9cfa13ce4c12cc
yapaluc <133736308+yapaluc@users.noreply.github.com>
2024-05-02 22:28:08 -0700

    Brew formula update for hg-git version v0.1.34

--- hg-git.rb
+++ hg-git.rb
@@ -5,20 +5,20 @@
 class HgGit < Formula
   desc ""
   homepage "https://github.com/yapaluc/hg-git"
-  version "0.1.33"
+  version "0.1.34"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.33/hg-git_Darwin_x86_64.tar.gz"
-      sha256 "cafc604950ba20a7554fd7fba4b735fb0d1bc51e7a4348e92af133e50a00e3fe"
+      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.34/hg-git_Darwin_x86_64.tar.gz"
+      sha256 "9f1b0c4b4b5e64cf2ee0f3779bddcffcd84f5ca2bee5a1341b4a2ab849cc36b1"
 
       def install
         bin.install "hg-git"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.33/hg-git_Darwin_arm64.tar.gz"
-      sha256 "baa0d6e31728e6ce5f50f48f846842b3edc782f974f8e56b468d5a92bd6cd188"
+      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.34/hg-git_Darwin_arm64.tar.gz"
+      sha256 "659e68a5490f39a208109d8126b49d3b1214ea1691b3ea858bc568c8c49f3e6f"
 
       def install
         bin.install "hg-git"
@@ -28,16 +28,16 @@ class HgGit < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.33/hg-git_Linux_x86_64.tar.gz"
-      sha256 "78afc93acbf7c7918b4901802759775251b7796fe59da889d7a9a0212716c0f0"
+      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.34/hg-git_Linux_x86_64.tar.gz"
+      sha256 "8877de064e47a047e57b2f87687a17c08b1883e6e6b6d54f7a73961c97954581"
 
       def install
         bin.install "hg-git"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.33/hg-git_Linux_arm64.tar.gz"
-      sha256 "5447cde1da1d1fc9a9c7b05265784ea6012f92b46dd5adf554b8aef72d856dbc"
+      url "https://github.com/yapaluc/hg-git/releases/download/v0.1.34/hg-git_Linux_arm64.tar.gz"
+      sha256 "0657be4a137b44b0724f232df0abcbe69d44360e1cf2b7e82cf862389be7a9fc"
 
       def install
         bin.install "hg-git"
